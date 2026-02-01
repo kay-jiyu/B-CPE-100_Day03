4cdf589edb5e0c736077fc68cbfa958d131badc2
自由Kay <venancekayode86@gmail.com>
2026-02-02 00:38:03 +0100

    Update my_print_digits.c

--- CPOOL_Day03_ACADEMIC2026/my_print_digits.c
+++ CPOOL_Day03_ACADEMIC2026/my_print_digits.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-void my_putchar(char a){
-    write(1, &a, 1);
-}
+
 
 int my_print_alpha(void){
     char a = '1';
@@ -12,11 +10,4 @@ int my_print_alpha(void){
          a++;
     };
 return 0;
-}
-
-int main(void)
-{
-    my_print_alpha();
-
-    return 0;
 }
