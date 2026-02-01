73c63fb0fcd21ee06862c4809c29ed37016e5d66
自由Kay <venancekayode86@gmail.com>
2026-02-02 00:32:44 +0100

    Update my_print_comb2.c

--- CPOOL_Day03_ACADEMIC2026/my_print_comb2.c
+++ CPOOL_Day03_ACADEMIC2026/my_print_comb2.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-void my_putchar(char a)
-{
-    write(1, &a, 1);
-}
+
 
 int my_print_comb()
 {
@@ -26,11 +23,4 @@ int my_print_comb()
         }
     }
     return 0; 
-}
-
-int main(void)
-{
-    my_print_comb();
-
-    return 0;
 }
