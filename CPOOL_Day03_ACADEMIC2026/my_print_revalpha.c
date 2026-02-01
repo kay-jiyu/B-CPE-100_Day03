18291d0e52602dd88a5d6e1b416b24bcf356dea5
自由Kay <venancekayode86@gmail.com>
2026-02-02 00:38:37 +0100

    Update my_print_revalpha.c

--- CPOOL_Day03_ACADEMIC2026/my_print_revalpha.c
+++ CPOOL_Day03_ACADEMIC2026/my_print_revalpha.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-void my_putchar(char a){
-    write(1, &a, 1);
-}
+
 
 int my_print_alpha(void){
     char letter = 'z';
@@ -14,9 +12,3 @@ int my_print_alpha(void){
 return 0;
 }
 
-int main(void)
-{
-    my_print_alpha();
-
-    return 0;
-}
