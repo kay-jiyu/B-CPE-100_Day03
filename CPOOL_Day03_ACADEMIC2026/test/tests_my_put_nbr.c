48d486946f79983b629983710291a318b42b5635
自由Kay <venancekayode86@gmail.com>
2026-02-02 02:07:52 +0100

    Create tests_my_put_nbr.c

---
+++ CPOOL_Day03_ACADEMIC2026/test/tests_my_put_nbr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+
+    my_put_nbr(0);
+    my_putchar('\n');
+    my_put_nbr(21);
+    my_putchar('\n');
+    my_put_nbr(-21);
+    my_putchar('\n');
+
+    return 0;
+}
